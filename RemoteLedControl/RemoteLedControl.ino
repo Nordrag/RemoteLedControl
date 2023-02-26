7538017445d39c8ae5565b908c24688dfac9d297
VirtualVoid <ivanboros893@gmail.com>
2023-02-26 20:55:11 +0100

    new timer

--- RemoteLedControl/RemoteLedControl.ino
+++ RemoteLedControl/RemoteLedControl.ino
@@ -13,145 +13,69 @@
 #include <Preferences.h>
 #define LOG(X) Serial.println(X)
 
-
 #pragma region Members
 
-const char* localHost = "http://localhost:5289/Devices";
-const char* azureServerDevicesUrl = "https://arduinowebapi.azure-api.net/Devices";
-const char* azureServerTimersUrl = "https://arduinowebapi.azure-api.net/Datetime";
 const char* azureServerBaseUrl = "https://arduinowebapi.azure-api.net/";
 
-StaticJsonDocument<256> doc;
-DeserializationError err;
-
-const int _measureUpdateRate = 5;
-int measurreUpdateRate = 5;
-
-String weekDays[7] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
-
-String months[12] = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
-
+//wifi connection
 WiFiUDP udp;
 NTPClient timeClient(udp);
 
+//token serialization
+StaticJsonDocument<256> doc;
+DeserializationError err;
+
+//hardware specs
 const int pumpOutput = 4;
 const int lightOutput = 16;
 const int heatSensor = 13;
 int deviceId;
-bool isPumpOn, isLightOn;
-bool isRequestOver = false;
-bool isTimerOn = false;
-bool lockOut = false;
-bool wasRequestBeforeTimer = false;
-DateTime Now(2022, 10, 10, 12, 0, 0);
-DateTime timer(2022, 10, 10, 12, 0, 0);
-DateTime onSetupTimer(2022, 10, 10, 12, 0, 0);
+
+//net connection
 WiFiManager wMan;
 String ssid, pw;
-WebServer server(80);
 
-OneWire oneWire(heatSensor);
-DallasTemperature sensors(&oneWire);
+//timers
+DateTime Now(2022, 10, 10, 12, 0, 0);
+DateTime pTimer(2022, 10, 10, 12, 0, 0);
+DateTime lTimer(2022, 10, 10, 12, 0, 0);
 
 int prevSecond = 0;
-bool shouldTick = false;
+bool pShouldTick = false;
+bool lShouldTick = false;
 
+const int _measureUpdateRate = 5;
+int measurreUpdateRate = 5;
 unsigned long current, old, longDeltaTime;
 int refreshRate = 5;
-int workTimeDelta;
-int year, month, day, h, m, s;
+int workTimeDelta, lightTimerDelta;
+int year, month, day, h, m, s, lYear, lMonth, lDay, lh,lm,ls;
 int currYear, currMonth, monthDay, currentHour, currentMinute, currentSeconds;
 struct tm* ptm;
 
+//measurements
+OneWire oneWire(heatSensor);
+DallasTemperature sensors(&oneWire);
+
 int temp = 0;
 float ph = 0;
-Preferences prefs;
 TaskHandle_t TemperatureReader;
+
+//lockouts
+bool wasPumpStatusSent = false;
+bool wasLightStatusSent = false;
+
+//persistence
+Preferences prefs;
 #pragma endregion
 
-#pragma region WebServer
-void Restart()
-{
-    server.send(200, "text/html", "pump on");
-    ESP.restart();
-}
-
-void GetAllStatus()
-{
-    server.send(200, "text/html", String(String(isPumpOn) + "|" + String(isLightOn) + "|" + String(isTimerOn)));
-}
-
-void PumpOn()
-{
-    digitalWrite(pumpOutput, LOW);
-    server.send(200, "text/html", "pump on");
-    isPumpOn = true;
-    LOG("on");
-}
-
-void GetTemperature()
-{
-    server.send(200, "text/html", String(temp));
-}
-
-void PumpOff()
-{
-    digitalWrite(pumpOutput, HIGH);
-    server.send(200, "text/html", "pump off");
-    isPumpOn = false;
-}
-
-void LightsOn()
-{
-    digitalWrite(lightOutput, LOW);
-    server.send(200, "text/html", "lighs on");
-    isLightOn = true;
-}
-
-void LightsOff()
-{
-    digitalWrite(lightOutput, HIGH);
-    server.send(200, "text/html", "lighs off");
-    isLightOn = false;
-}
-
-void OnConnect()
-{
-    server.send(200, "text/html", "base url");
-}
-
-void SendPumpStatus()
-{
-    server.send(200, "text/html", isPumpOn ? "on" : "off");
-}
-
-void SendLightStatus()
-{
-    server.send(200, "text/html", isLightOn ? "on" : "off");
-}
-
-void NotFound()
-{
-    server.send(404, "text/html", "not found");
-}
-
-void SendTimerStatus()
-{
-    server.send(200, "text/html", isTimerOn ? "on" : "off");
-}
-
-void SendTemperature()
-{
-    server.send(200, "text/html", String(temp));
-}
-
-#pragma endregion
-
+#pragma region New
+//should be called with frequency of X
 void GetStatusFromServer()
 {
     HTTPClient client;
     String requestUrl = azureServerBaseUrl;
-    requestUrl += "/Status?id=";
+    requestUrl += "/fullStatus?id=";
     requestUrl += deviceId;
     client.begin(requestUrl);
     client.addHeader("accept", "application/json");
@@ -160,121 +84,47 @@ void GetStatusFromServer()
     String payload = client.getString();        
     if (code > 0)
     {
-        err = deserializeJson(doc, payload);      
-        isPumpOn = doc["isPumpOn"];
-        isLightOn = doc["isLightOn"];
-        isTimerOn = doc["isTimerOn"];
-        refreshRate = doc["refreshRate"];
-        temp = doc["temperature"];
-        ph = doc["ph"];
-       
-        if (isTimerOn == 1 && shouldTick)
-        {
-           
-        }  
-        else
-        {
-            workTimeDelta = doc["maxRunTime"];
-            workTimeDelta *= 60;
-        }     
-    }
-    else
-    {
-        refreshRate = 3000;
-    }    
-    client.end();
-    GetCurrentTimer();
-}
+        err = deserializeJson(doc, payload);  
 
-//void GetRemainingTime()
-//{
-//    server.send(200, "text/html", "remTime");
-//    HTTPClient client;
-//    String requestUrl = azureServerTimersUrl;
-//    requestUrl += "/getRemainingTime?deviceId=";
-//    requestUrl += deviceId;
-//    client.begin(requestUrl);
-//    client.addHeader("accept", "application/json");
-//    client.addHeader("Content-Type", "text/plain");
-//    auto code = client.GET();
-//    if (code > 0)
-//    {
-//        String payload = client.getString();
-//        err = deserializeJson(doc, payload);
-//        String d = doc["time"];
-//        workTimeDelta = d.toInt() * 60;
-//        isTimerOn = doc["isActive"];
-//    }
-//    client.end();
-//}
-
-//void ToggleTimer()
-//{
-//    GetRemainingTime();  
-//}
-
-void GetCurrentTimer()
-{      
-    HTTPClient client;
-    String request = azureServerTimersUrl;
-    server.send(200, "text/html", "getting current time");
-    request += "/getCurrentTimer?deviceId=";
-    request += deviceId;
-    client.begin(request);
-    client.addHeader("accept", "application/json");
-    client.addHeader("Content-Type", "text/plain");
-    int code = client.GET();
-    String payload = client.getString();                 
-    if (code > 0)
-    {       
-        err = deserializeJson(doc, payload);
-        year = doc["year"];
-        month = doc["month"];
-        day = doc["day"];
-        h = doc["hour"];
-        m = doc["minutes"];
-        s = doc["seconds"];           
-        timer.UpdateTime(year, month, day, h, m, s);         
-    }
-    client.end();   
-}
-
-void GetNextTimer()
-{  
-    server.send(200, "text/html", "nextTime");
-    HTTPClient client;
-    String requestUrl = azureServerTimersUrl;
-    requestUrl += "/minutesDebug?deviceId=";
-    requestUrl += deviceId;
-    client.begin(requestUrl);
-    client.addHeader("accept", "application/json");
-    client.addHeader("Content-Type", "text/plain");
-    auto code = client.GET();
-
-    String payload = client.getString();    
-    //LOG(payload);
-    if (code > 0)
-    {
-        err = deserializeJson(doc, payload);
+        //pump timer
         year = doc["year"];
         month = doc["month"];
         day = doc["day"];
         h = doc["hour"];
         m = doc["minutes"];
         s = doc["seconds"];
-        workTimeDelta = doc["workTime"];
-        isTimerOn = doc["isTimerOn"];
-        workTimeDelta *= 60;
-        timer.UpdateTime(year, month, day, h, m, s);
-        wasRequestBeforeTimer = false;              
+        bool isPumpActive = doc["IsActive"];
+
+        //light timer
+        lYear = doc["lyear"];
+        lMonth = doc["lmonth"];
+        lDay = doc["lday"];
+        lh = doc["lhour"];
+        lm = doc["lminutes"];
+        ls = doc["lseconds"];
+        bool isLightActive = doc["IsLightActive"];
+
+        pTimer.UpdateTime(year, month, day, h, m, s);
+        lTimer.UpdateTime(lYear, lMonth, lDay, lh, lm, ls);
+
+        refreshRate = doc["refreshRate"];           
+        workTimeDelta = isPumpActive? doc["RemainingPumpTime"] * 60 : 0; 
+        lightTimerDelta = isLightActive? doc["RemainingLightTime"] * 60 : 0;
     }
-    client.end();
+    else
+    {
+        refreshRate = 3000;
+        workTimeDelta = 0;
+        lightTimerDelta = 0;
+    }    
+    client.end();  
 }
 
 void GetDeviceID()
 {
     HTTPClient client;
-    String requestUrl = azureServerDevicesUrl;
+    String requestUrl = azureServerBaseUrl;
+    requestUrl += "Devices";
     requestUrl += "/getId?SSID=";
     requestUrl += ssid;
     requestUrl += "&Password=";
@@ -307,13 +157,14 @@ void PostNewDevice()
     jObj["ip"] = WiFi.localIP().toString();
     serializeJson(pRequestDoc, jResult);
     HTTPClient client;
-    client.begin(azureServerDevicesUrl);
+    client.begin(azureServerBaseUrl + String("Devices"));
     client.addHeader("accept", "text/plain");
     client.addHeader("Content-Type", "application/json");
     int httpCode = client.POST(jResult);
     client.end();
 }
 
+//updates the current time, used for comparing to server timers
 void UpdateDateTime()
 {
     timeClient.setTimeOffset(3600);
@@ -326,28 +177,13 @@ void UpdateDateTime()
 
     currentHour = timeClient.getHours();
     currentMinute = timeClient.getMinutes();
-    currentSeconds = timeClient.getSeconds();
-
-    if (currentSeconds != prevSecond)
-    {
-        refreshRate--;
-        measurreUpdateRate--;
-        prevSecond = currentSeconds;         
-        if (isTimerOn == 1 && shouldTick)
-        {
-            workTimeDelta--;
-            LOG(workTimeDelta);
-        }
-        if (refreshRate <= 0)
-        {
-            GetStatusFromServer();
-        }
-    }
+    currentSeconds = timeClient.getSeconds();   
 
     //String weekDay = weekDays[timeClient.getDay()];   
     Now.UpdateTime(currYear, currMonth, monthDay, currentHour, currentMinute, currentSeconds);
 }
 
+//send the temp and ph to the server
 void SendMeasuresToServer()
 {
     StaticJsonDocument<256> pRequestDoc;
@@ -368,17 +204,15 @@ void SendMeasuresToServer()
     client.end();
 }
 
+//sends the activity status to the server
 void SendStatusToServer()
 {
     StaticJsonDocument<256> pRequestDoc;
     JsonObject jObj = pRequestDoc.to<JsonObject>();
     String jResult;
-    jObj["id"] = deviceId;
-    jObj["temperature"] = temp;
-    jObj["ph"] = ph;
-    jObj["isPumpOn"] = isPumpOn;
-    jObj["isLightOn"] = isLightOn;
-    jObj["isTimerOn"] = isTimerOn;
+    jObj["id"] = deviceId;   
+    jObj["isPumpOn"] = workTimeDelta > 0;
+    jObj["isLightOn"] = lightTimerDelta > 0;   
     serializeJson(pRequestDoc, jResult);
     HTTPClient client;
     String url = azureServerBaseUrl;
@@ -387,16 +221,11 @@ void SendStatusToServer()
     client.addHeader("accept", "text/plain");
     client.addHeader("Content-Type", "application/json");
     int httpCode = client.PUT(jResult);
-
-    if (httpCode < 0)
-    {
-        isPumpOn = false;
-        isTimerOn = false;
-        workTimeDelta = 0;
-    }
+    LOG(jResult);    
     client.end();
 }
 
+//runs on the 2nd thread taking measurements
 void UpdateMeasures(void* param)
 {
     while (true)
@@ -407,33 +236,59 @@ void UpdateMeasures(void* param)
     }
 }
 
+#pragma endregion
+
 void StateUpdate()
 {
-    shouldTick = DateTime::CompareTime(&Now, &timer);
-    if (isTimerOn == 1)
+    pShouldTick = DateTime::CompareTime(&Now, &pTimer);
+    lShouldTick = DateTime::CompareTime(&Now, &lTimer);
+
+
+    if (currentSeconds != prevSecond)
     {
-        if (shouldTick)
+        //auto refresh delta
+        refreshRate--;
+        measurreUpdateRate--;
+        prevSecond = currentSeconds;
+        //pump toggle
+        if (workTimeDelta > 0 && pShouldTick)
         {
-            if (!lockOut)
+            workTimeDelta--;
+            LOG(workTimeDelta);
+            digitalWrite(pumpOutput, LOW);           
+        }
+        else
+        {
+            digitalWrite(pumpOutput, HIGH);
+            if (!wasPumpStatusSent)
             {
-                lockOut = true;
                 SendStatusToServer();
-                LOG("timer on");
-            }
-            isPumpOn = true;
-            digitalWrite(pumpOutput, LOW);
-            if (workTimeDelta <= 0)
-            {
-                LOG("timer off");
-                isPumpOn = false;
-                SendStatusToServer();
-                lockOut = false;
-                GetNextTimer();
-                digitalWrite(pumpOutput, HIGH);
+                wasPumpStatusSent = true;
             }
         }
-    }
-  
+
+        //light toggle
+        if (lightTimerDelta > 0 && lShouldTick)
+        {
+            lightTimerDelta--;
+            digitalWrite(lightOutput, LOW);
+        }
+        else
+        {
+            digitalWrite(lightOutput, HIGH);
+            if (!wasLightStatusSent)
+            {
+                SendStatusToServer();
+                wasLightStatusSent = true;
+            }
+        }
+
+        //auto refresh call
+        if (refreshRate <= 0)
+        {
+            GetStatusFromServer();
+        }
+    } 
 }
 
 void setup() {
@@ -462,36 +317,17 @@ void setup() {
     timeClient.begin();
     PostNewDevice();
     GetDeviceID();
-    GetCurrentTimer();
     UpdateDateTime();
     GetStatusFromServer();
-    onSetupTimer.UpdateTime(currYear, currMonth, monthDay, currentHour, currentMinute, 0);
-    wasRequestBeforeTimer = DateTime::CompareDayTime(&onSetupTimer, &timer);
-    
-    server.on("/", OnConnect);
-    server.on("/pumpOn", PumpOn);
-    server.on("/pumpOff", PumpOff);
-    server.on("/dateTime", GetCurrentTimer);   
-    server.on("/status", SendPumpStatus);   
-    server.on("/temp", SendTemperature);   
-    server.on("/lightOn", LightsOn);   
-    server.on("/lightOff", LightsOff);   
-    server.on("/lightStatus", SendLightStatus);
-    //server.on("/toggleTimer", ToggleTimer);
-    server.on("/timerStatus", SendTimerStatus);
-    server.on("/allStatus", GetAllStatus);
-    server.on("/restart", Restart);
-    server.onNotFound(NotFound);   
-    server.begin(); 
 
+    LOG(WiFi.localIP().toString());
     xTaskCreatePinnedToCore(UpdateMeasures, "TempReader", 10000, nullptr, 1, &TemperatureReader, 1);
 }
 
 void loop() {
 
     timeClient.update();   
-    UpdateDateTime();
-    server.handleClient();     
+    UpdateDateTime();    
     StateUpdate();    
 
     if (measurreUpdateRate <= 0)
